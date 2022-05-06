0214347ff4377e70a76900424dd4f25f4b38c769
rabaig <57613056+rabaig@users.noreply.github.com>
2022-05-06 19:53:59 -0400

    Add files via upload

---
+++ label_dict.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+
+from libs.configs import cfgs
+
+
+class_names = [
+        'back_ground', 'person', 'bicycle', 'car', 'motorcycle',
+        'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+        'fire hydrant', 'stop sign', 'parking meter', 'bench',
+        'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant',
+        'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+        'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
+        'sports ball', 'kite', 'baseball bat', 'baseball glove',
+        'skateboard', 'surfboard', 'tennis racket', 'bottle',
+        'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+        'banana', 'apple', 'sandwich', 'orange', 'broccoli',
+        'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
+        'couch', 'potted plant', 'bed', 'dining table', 'toilet',
+        'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+        'microwave', 'oven', 'toaster', 'sink', 'refrigerator',
+        'book', 'clock', 'vase', 'scissors', 'teddy bear',
+        'hair drier', 'toothbrush']
+
+classes_originID = {
+    'person': 1, 'bicycle': 2, 'car': 3, 'motorcycle': 4,
+    'airplane': 5, 'bus': 6, 'train': 7, 'truck': 8, 'boat': 9,
+    'traffic light': 10, 'fire hydrant': 11, 'stop sign': 13,
+    'parking meter': 14, 'bench': 15, 'bird': 16, 'cat': 17,
+    'dog': 18, 'horse': 19, 'sheep': 20, 'cow': 21, 'elephant': 22,
+    'bear': 23, 'zebra': 24, 'giraffe': 25, 'backpack': 27,
+    'umbrella': 28, 'handbag': 31, 'tie': 32, 'suitcase': 33,
+    'frisbee': 34, 'skis': 35, 'snowboard': 36, 'sports ball': 37,
+    'kite': 38, 'baseball bat': 39, 'baseball glove': 40,
+    'skateboard': 41, 'surfboard': 42, 'tennis racket': 43,
+    'bottle': 44, 'wine glass': 46, 'cup': 47, 'fork': 48,
+    'knife': 49, 'spoon': 50, 'bowl': 51, 'banana': 52, 'apple': 53,
+    'sandwich': 54, 'orange': 55, 'broccoli': 56, 'carrot': 57,
+    'hot dog': 58, 'pizza': 59, 'donut': 60, 'cake': 61,
+    'chair': 62, 'couch': 63, 'potted plant': 64, 'bed': 65,
+    'dining table': 67, 'toilet': 70, 'tv': 72, 'laptop': 73,
+    'mouse': 74, 'remote': 75, 'keyboard': 76, 'cell phone': 77,
+    'microwave': 78, 'oven': 79, 'toaster': 80, 'sink': 81,
+    'refrigerator': 82, 'book': 84, 'clock': 85, 'vase': 86,
+    'scissors': 87, 'teddy bear': 88, 'hair drier': 89,
+    'toothbrush': 90}
+
+
+def get_coco_label_dict():
+    originID_classes = {item: key for key, item in classes_originID.items()}
+    NAME_LABEL_MAP = dict(zip(class_names, range(len(class_names))))
+    return NAME_LABEL_MAP
+
+
+if cfgs.DATASET_NAME == 'WIDER':
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'face': 1
+    }
+elif cfgs.DATASET_NAME == 'ICDAR2015':
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'text': 1
+    }
+elif cfgs.DATASET_NAME == 'HRSC2016':
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'ship': 1
+    }
+elif cfgs.DATASET_NAME == 'UCAS-AOD':
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'car': 1,
+        'plane': 2
+    }
+elif cfgs.DATASET_NAME.startswith('DOTA'):
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'roundabout': 1,
+        'tennis-court': 2,
+        'swimming-pool': 3,
+        'storage-tank': 4,
+        'soccer-ball-field': 5,
+        'small-vehicle': 6,
+        'ship': 7,
+        'plane': 8,
+        'large-vehicle': 9,
+        'helicopter': 10,
+        'harbor': 11,
+        'ground-track-field': 12,
+        'bridge': 13,
+        'basketball-court': 14,
+        'baseball-diamond': 15
+    }
+    if cfgs.DATASET_NAME == 'DOTA1.5':
+        NAME_LABEL_MAP['container-crane'] = 16
+
+elif cfgs.DATASET_NAME == 'coco':
+    NAME_LABEL_MAP = get_coco_label_dict()
+elif cfgs.DATASET_NAME == 'pascal':
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'aeroplane': 1,
+        'bicycle': 2,
+        'bird': 3,
+        'boat': 4,
+        'bottle': 5,
+        'bus': 6,
+        'car': 7,
+        'cat': 8,
+        'chair': 9,
+        'cow': 10,
+        'diningtable': 11,
+        'dog': 12,
+        'horse': 13,
+        'motorbike': 14,
+        'person': 15,
+        'pottedplant': 16,
+        'sheep': 17,
+        'sofa': 18,
+        'train': 19,
+        'tvmonitor': 20
+    }
+elif cfgs.DATASET_NAME.startswith('OCR'):
+    NAME_LABEL_MAP = {
+    	# 'QIB': 0,
+        # 'QIC': 1,
+        # 'QIFO': 2,
+        # 'QIGO': 3,
+        # 'QIHO': 4,
+        # 'QIIO': 5,
+        # 'QIJO': 6,
+        # 'QIMO': 7,
+        # 'QINO': 8,
+        # 'QIOO': 9,
+        # 'QIPO': 10,
+        # 'QIQO': 11,
+        # 'QI1O': 12,
+        # 'QI3O': 13,
+        # 'QI4O': 14,
+        # 'QJA0': 15,
+        # 'QJF0': 16,
+        # 'QJM0': 17,
+        # 'QJOO': 18,
+        # 'QJRO': 19,
+        # 'QJTO': 20,
+        # 'QJUO': 21,
+        # 'QJYO': 22,
+        # 'QJ1O': 23,
+        # 'QKAO': 24,
+        # 'QKBO': 25,
+        # 'QKCO': 26,
+        # 'QKDO': 27
+        'A': 0,
+        'B': 1,
+        'C':2,
+        'D':3,
+        'E':4,
+        'F':5,
+        'G':6,
+        'H':7,
+        'I':8,
+        'J':9,
+        'K':10,
+        'L':11,
+        'M':12,
+        'N':13,
+        'O':14,
+        'P':15,
+        'Q':16,
+        'R':17,
+        'S':18,
+        'T':19,
+        'U':20,
+        'V':21,
+        'W':22,
+        'X':23,
+        'Y':24,
+        'Z':25,
+        '1':26,
+        '2':27,
+        '3':28,
+        '4':29,
+        '5':30,
+        '6':31,
+        '7':32,
+        '8':33,
+        '9':34,
+        '0':35,
+        'AA':36,
+        'BB':37,
+        'CC':38,
+        'DD':39,
+        'EE':40,
+        'FF':41,
+        'GG':42,
+        'HH':43,
+        'II':44,
+        'JJ':45,
+        'KK':46,
+        'LL':47,
+        'MM':48,
+        'NN':49,
+        'OO':50,
+        'PP':51,
+        'QQ':52,
+        'RR':53,
+        'SS':54,
+        'TT':55,
+        'UU':56,
+        'VV':57,
+        'WW':58,
+        'XX':59,
+        'YY':60,
+        'ZZ':61,
+        '11':62,
+        '22':63,
+        '33':64,
+        '44':65,
+        '55':66,
+        '66':67,
+        '77':68,
+        '88':69,
+        '99':70,
+        '00':71
+    }
+elif cfgs.DATASET_NAME == 'bdd100k':
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'bus': 1,
+        'traffic light': 2,
+        'traffic sign': 3,
+        'person': 4,
+        'bike': 5,
+        'truck': 6,
+        'motor': 7,
+        'car': 8,
+        'train': 9,
+        'rider': 10
+    }
+else:
+    assert 'please set label dict!'
+
+
+def get_label_name_map():
+    reverse_dict = {}
+    for name, label in NAME_LABEL_MAP.items():
+        reverse_dict[label] = name
+    return reverse_dict
+
+
+LABEL_NAME_MAP = get_label_name_map()
